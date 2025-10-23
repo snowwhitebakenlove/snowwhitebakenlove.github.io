2e121be3a17d4ec42c2cce7e08129aaa48be52a6
snowwhitebakenlove <susan.nguyen2203@gmail.com>
2025-10-23 10:48:34 -0400

    update JS

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ particlesJS('particles-js',
       }
     },
     "color": {
-      "value": "#ff3232"
+      "value": "#eec1ad"
     },
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#ff3232"
+        "color": "#eec1ad"
       },
       "polygon": {
         "nb_sides": 5
